ed67e14812dba92c39d0fbbd472cd6ad17909513
Investmentbanking <ayesha.dorani@kcl.ac.uk>
2023-09-23 22:54:13 +0100

    Calculate prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,30 @@
 
 def primes(number_of_primes):
     list = []
+
+    total = 0
+    start = 2
+    numbers = 0
+
+    while total < number_of_primes:
+
+        numbers = 0
+
+        if start == 2:
+            list.append(start)
+            start+=1
+            total+=1
+            continue
+
+        for x in range(1, start+1):
+            if(start % x == 0):
+                numbers+=1
+        
+        if numbers == 2:
+            list.append(start)
+            start+=1
+            total+=1
+        else:
+            start+=1
+
     return list
